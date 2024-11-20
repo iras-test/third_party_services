867ddab2e90831dc4c7f8cab2116d6bddfc3aa2b
Sunday Deogratias <sundaydeogratias8@gmail.com>
2024-11-20 21:35:11 +0300

    debug

--- third_party_services/serializers.py
+++ third_party_services/serializers.py
@@ -12,44 +12,44 @@ class NinDetailsSerializer(serializers.Serializer):
 
         resp = validate_nin(nin)
 
-        try:
+        # try:
 
-            if resp.status == 500:
-                raise serializers.ValidationError({'detail': 'Internal server error'})
-            
-            elif resp.status == 400:
-                resp_data = json.loads(resp.data.decode('utf-8'))
-                raise serializers.ValidationError({'detail': resp_data.get('errorDesc')})
-
-            elif resp.status == 200:
-                resp_data = json.loads(resp.data.decode('utf-8'))
-                if resp_data['niraDataAsync'].get('isValid') == True:
-
-                    personNationalId =  resp_data['niraDataAsync'].get('personNationalId', '')
-                    personSurname =  resp_data['niraDataAsync'].get('personSurname', '')
-                    personGivenNames =  resp_data['niraDataAsync'].get('personGivenNames', '')
-                    personMaidenNames = resp_data['niraDataAsync'].get('personMaidenNames', '')
-                    personOtherNames =  resp_data['niraDataAsync'].get('personOtherNames', '')
-                    personDateOfBirth =  resp_data['niraDataAsync'].get('personDateOfBirth', '')
-                    personNationality = resp_data['niraDataAsync'].get('personNationality', '')
-                    transactionStatus = resp_data['niraDataAsync'].get('transactionStatus', '')
-                    isValid = resp_data['niraDataAsync'].get('isValid', '')
-                    message = resp_data['niraDataAsync'].get('message', '')
-
-                    return {
-                        "personNationalId": personNationalId,
-                        "personSurname": personSurname,
-                        "personGivenNames": personGivenNames,
-                        "personMaidenNames": personMaidenNames if unmasked else  mask_string(personMaidenNames),
-                        "personOtherNames": personOtherNames if unmasked else  mask_string(personOtherNames),
-                        "personDateOfBirth": personDateOfBirth if unmasked else  mask_string(personDateOfBirth, 1),
-                        "personNationality": personNationality if unmasked else  mask_string(personNationality, 1),
-                    }
-                
-                else:
-                    raise serializers.ValidationError({'detail': resp_data.get('errorS')[0]['errorDesc']})
-
-            return resp
+        if resp.status == 500:
+            raise serializers.ValidationError({'detail': 'Internal server error'})
         
-        except:
-            return []
+        elif resp.status == 400:
+            resp_data = json.loads(resp.data.decode('utf-8'))
+            raise serializers.ValidationError({'detail': resp_data.get('errorDesc')})
+
+        elif resp.status == 200:
+            resp_data = json.loads(resp.data.decode('utf-8'))
+            if resp_data['niraDataAsync'].get('isValid') == True:
+
+                personNationalId =  resp_data['niraDataAsync'].get('personNationalId', '')
+                personSurname =  resp_data['niraDataAsync'].get('personSurname', '')
+                personGivenNames =  resp_data['niraDataAsync'].get('personGivenNames', '')
+                personMaidenNames = resp_data['niraDataAsync'].get('personMaidenNames', '')
+                personOtherNames =  resp_data['niraDataAsync'].get('personOtherNames', '')
+                personDateOfBirth =  resp_data['niraDataAsync'].get('personDateOfBirth', '')
+                personNationality = resp_data['niraDataAsync'].get('personNationality', '')
+                transactionStatus = resp_data['niraDataAsync'].get('transactionStatus', '')
+                isValid = resp_data['niraDataAsync'].get('isValid', '')
+                message = resp_data['niraDataAsync'].get('message', '')
+
+                return {
+                    "personNationalId": personNationalId,
+                    "personSurname": personSurname,
+                    "personGivenNames": personGivenNames,
+                    "personMaidenNames": personMaidenNames if unmasked else  mask_string(personMaidenNames),
+                    "personOtherNames": personOtherNames if unmasked else  mask_string(personOtherNames),
+                    "personDateOfBirth": personDateOfBirth if unmasked else  mask_string(personDateOfBirth, 1),
+                    "personNationality": personNationality if unmasked else  mask_string(personNationality, 1),
+                }
+            
+            else:
+                raise serializers.ValidationError({'detail': resp_data.get('errorS')[0]['errorDesc']})
+
+        return resp
+        
+        # except:
+        #     return []
