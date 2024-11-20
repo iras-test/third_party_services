19f913bc6ebf9b9a8acf5d4dd475e2f69f4aa079
Sunday Deogratias <sundaydeogratias8@gmail.com>
2024-11-20 21:26:21 +0300

    Change naming

--- third_party_services/serializers.py
+++ third_party_services/serializers.py
@@ -1,6 +1,6 @@
 import json
 from rest_framework import serializers
-from third_party_services.actions import validate_nin
+from third_party_services.helpers import validate_nin
 
 
 class NinDetailsSerializer(serializers.Serializer):
