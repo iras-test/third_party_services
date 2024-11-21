401619e3ca73119b7d3acb314999a13cd26fda1a
Sunday Deogratias <sundaydeogratias8@gmail.com>
2024-11-21 10:49:55 +0300

    Add functionality for getting vehicle details

--- third_party_services/actions.py
+++ third_party_services/actions.py
@@ -26,3 +26,24 @@ def validate_vehicle(self, request, **kwargs):
     serializer = VehicleDetailsSerializer(data=request.query_params)
     serializer.is_valid(raise_exception=True)
     return Response(serializer.details())
+
+def get_vehicle_details(number_plate):
+    vehicle_serializer = VehicleDetailsSerializer(data={'number_plate': number_plate})
+    vehicle_details = vehicle_serializer.details(unmasked=True)
+
+    tin = vehicle_details.get('tin', None)
+    email = vehicle_details.get('email', None)
+    mobile_number = vehicle_details.get('mobile_number', None)
+    owner_name = vehicle_details.get('tax_payer_name', "")
+
+    make_year =  vehicle_details.get('make_year', "")
+    model = vehicle_details.get('model', "")
+    engine_number = vehicle_details.get('engine_number', "")
+    chasis_number = vehicle_details.get('chasis_number', "")
+    color = vehicle_details.get('color', "")
+    is_individual = vehicle_details.get('is_individual', "")
+    seat_capacity =  vehicle_details.get('seat_capacity', "")
+    category_name =  vehicle_details.get('category_name', "")
+    purpose =  vehicle_details.get('purpose', "")
+    
+    return owner_name, f"256{mobile_number}", email, tin, make_year , model, engine_number ,chasis_number ,color ,is_individual ,seat_capacity ,category_name, purpose
