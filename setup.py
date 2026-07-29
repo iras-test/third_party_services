eeaa9f7aeee80308a9d2c73754371ed0200a2edb
Dickens <dickenzkongoro@gmail.com>
2026-07-30 01:20:06 +0300

    bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='third_party_services',
-    version='0.1.8',
+    version='0.1.9',
     author='Sunday Deogratias',
     author_email='sundaydeogratias8@gmail.com',
     description='A wrapper package for YTL external services',
